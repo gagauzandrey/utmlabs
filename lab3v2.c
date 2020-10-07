4a522bfa548e8c9503d6f347d18a1cc6f40a7c3c
Andrey Gagauz <simpleandrey24@gmail.com>
2020-10-07 20:46:47 +0300

    Final version lab3v2.c

--- lab3v2.c
+++ lab3v2.c
@@ -8,21 +8,13 @@ int _get_random_range(int lower, int upper) {
     return lower + ( rand() % (upper - lower) );
 }
 
-
 int main()
 {
-    const int MAX_ROWS = 6;
-    const int MAX_COLUMNS = 9;
+    const int MAX_ROWS = 4;
+    const int MAX_COLUMNS = 5;
     int m[MAX_ROWS][MAX_COLUMNS];
     int i, j;
    
-/*     for (i = 0; i < MAX_ROWS; ++i) {
-        for (j = 0; j < MAX_COLUMNS; ++j) {
-            printf("m[%d][%d] = ", i + 1, j + 1);
-            scanf("%d", &m[i][j]);
-        }
-        printf("\n");
-    }  */
     srand( time(0) );
     for (i = 0; i < MAX_ROWS; ++i) {
         for (j = 0; j < MAX_COLUMNS; ++j) {
@@ -50,7 +42,7 @@ int main()
 
     printf("Кол-во столбов без 0: %d\n\n", counter); 
 
-    int tmp;
+
     int sum[MAX_ROWS];
     int k;
     i = 0;
@@ -67,34 +59,15 @@ int main()
         printf("Сумма %d-ой строки: %d\n", i + 1, sum[i]);
     }
     printf("\n");
-    
 
-/*     // i и j циклы проверяют суммы строк
-    int flag = 0; // отвечает за индекс строк при сортировке
-    int secFlag = 1; // отвечает за индекс +1 строки от flag
-    for (i = 0; i < MAX_ROWS; ++i) {
-        for (j = i + 1; j < MAX_ROWS; ++j) {
-            if (sum[i] > sum[j]) {
-                // k цикл меняет местами строки 
-                // int k - отвечает за столбцы 
-                for (k = 0; k < MAX_COLUMNS; ++k) {
-                    tmp = m[flag][k];
-                    m[flag][k] = m[secFlag][k];
-                    m[secFlag][k] = tmp;
-                } 
-                ++secFlag;
-                ++flag;   
-            }  
-        }    
-    }  */
-
+    int tmp;
     for (int i = 0; i < MAX_ROWS; ++i) {
         for (int j = MAX_ROWS - 1; j >= i; j--) {
             if (sum[j] < sum[j - 1]) {
                 for (int k = 0; k < MAX_COLUMNS; ++k) {
                     tmp = m[j][k];
                     m[j][k] = m[j - 1][k];
-                    m[j - 1][k] = tmp;
+                    m[j - 1][k] = tmp;  
                 }
                 tmp = sum[j];
                 sum[j] = sum[j - 1];
@@ -103,7 +76,7 @@ int main()
             }
     }
 
-    printf("Отсортированная по возрастанию характеристики: ");
+    printf("Отсортированные по возрастанию характеристики: ");
     for (i = 0; i < MAX_ROWS; ++i) {
         printf("%3d", sum[i]);
     }
